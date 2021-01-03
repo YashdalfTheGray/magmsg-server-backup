680c48438f900b562d0ec7c4b7a93ec5b0664ba4
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-03 01:47:55 -0500

    added some dummy middlewares to get the structure up and running

---
+++ server/middlewares.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+const getMessage = (req: Request, res: Response) => {};
+
+const putMessage = (req: Request, res: Response) => {};
+
+export { getMessage, putMessage };
