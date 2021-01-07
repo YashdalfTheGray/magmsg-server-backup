de90228a4f026780aa36b8e1bf8f628419d2570c
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-01-07 05:40:27 -0500

    added a middleware that uses the readall

--- server/middlewares.ts
+++ server/middlewares.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from 'express';
 
 import { getCredsFromEnvironment, getDynamoDbClient } from './sdk';
-import { getMessageById, putMessage } from './dal';
+import { getMessageById, getAllMessages, putMessage } from './dal';
+
+const getMessagesHandler = (TableName: string) => async (
+  req: Request,
+  res: Response
+) => {
+  if (req.app.locals.expiration.getTime() < Date.now()) {
+    const newCreds = await getCredsFromEnvironment();
+    req.app.locals.client = getDynamoDbClient(
+      newCreds.accessKeyId || '',
+      newCreds.secretAccessKey || '',
+      newCreds.sessionToken || ''
+    );
+    req.app.locals.expiration = newCreds.expiration || new Date();
+  }
+
+  const items = await getAllMessages(req.app.locals.client, TableName);
+
+  res.json(items);
+};
 
 const getMessageHandler = (tableName: string) => async (
   req: Request,
